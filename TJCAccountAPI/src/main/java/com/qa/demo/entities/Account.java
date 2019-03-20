1bf4d244b059bd71a75fcdb7ba0d73db8651c876
Luke <Lukeajones1996@gmail.com>
2019-03-20 12:06:19 +0000

    Added ID getter in account

--- TJCAccountAPI/src/main/java/com/qa/demo/entities/Account.java
+++ TJCAccountAPI/src/main/java/com/qa/demo/entities/Account.java
@@ -6,41 +6,50 @@ import javax.persistence.Id;
 
 @Entity
 public class Account {
-	
+
 	public Account(String firstName, String lastName) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
 	}
-	
+
 	public Account() {
-		
+
 	}
+
 	@Id
 	@GeneratedValue
 	private long id;
 	private String firstName;
 	private String lastName;
 	private String accountNum;
-	
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getSecondName() {
 		return lastName;
 	}
+
 	public void setSecondName(String surname) {
 		this.lastName = surname;
 	}
+
 	public String getAccountNum() {
 		return accountNum;
 	}
+
 	public void setAccountNum(String accountNum) {
 		this.accountNum = accountNum;
 	}
 	
+	public long getID() {
+		return id;
+	}
 
 }
