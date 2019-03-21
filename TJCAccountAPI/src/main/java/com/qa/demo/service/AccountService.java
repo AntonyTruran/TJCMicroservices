e491eef3798550ab686dc074ac65b7a0649c39b9
smurfjames101 <smurfjames@gmail.com>
2019-03-21 17:28:05 +0000

    Added Active MQ Integration
    Added Mongo Integration
    Both Send Messages Through ActiveMQ

--- TJCAccountAPI/src/main/java/com/qa/demo/service/AccountService.java
+++ TJCAccountAPI/src/main/java/com/qa/demo/service/AccountService.java
@@ -3,6 +3,8 @@ package com.qa.demo.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Service;
 
 import com.qa.demo.entities.Account;
@@ -12,6 +14,7 @@ import com.qa.demo.repository.AccountRepo;
 public class AccountService {
 
 	private AccountRepo accountRepo;
+	private JmsTemplate jmsTemplate;
 
 	public AccountService(AccountRepo accountRepo) {
 		this.accountRepo = accountRepo;
@@ -19,6 +22,7 @@ public class AccountService {
 
 	public void createAccount(Account account) {
 		this.accountRepo.save(account);
+		
 	}
 
 	public List<Account> getAccounts() {
@@ -32,5 +36,5 @@ public class AccountService {
 	public List<Account> accountSearch(Account account) {
 		return this.accountRepo.findAll().stream().filter(x -> x.matches(account)).collect(Collectors.toList());
 	}
-
+	
 }
