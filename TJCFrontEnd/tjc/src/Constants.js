f1448e78d8963c317d1a0085b378cc5f8547b42d
smurfjames101 <smurfjames@gmail.com>
2019-03-21 16:50:33 +0000

    Fixed Issue With Name And Account Number Loading

--- TJCFrontEnd/tjc/src/Constants.js
+++ TJCFrontEnd/tjc/src/Constants.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.FIRST_NAME = "http://localhost:8080/getLastAccountNames";
+exports.FIRST_NAME = "http://localhost:8080/getLastAccountName";
 exports.ACCOUNT_NUMBER = "http://localhost:8080/getLastAccountNumber";
-exports.CREATE_ACCOUNT = "";
+exports.CREATE_ACCOUNT = "http://localhost:8080/createAccount";
 //# sourceMappingURL=Constants.js.map
