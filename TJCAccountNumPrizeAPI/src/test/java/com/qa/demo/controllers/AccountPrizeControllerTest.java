4ed44459379364618778293412da7d4e02c05e7e
smurfjames101 <smurfjames@gmail.com>
2019-03-21 14:32:38 +0000

    Changed Strings To Use Constants

--- TJCAccountNumPrizeAPI/src/test/java/com/qa/demo/controllers/AccountPrizeControllerTest.java
+++ TJCAccountNumPrizeAPI/src/test/java/com/qa/demo/controllers/AccountPrizeControllerTest.java
@@ -5,58 +5,56 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.qa.sanity.Constants;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AccountPrizeControllerTest {
 	private AccountPrizeController controller;
-	
-	private static final String STARTS_A = "Unfortunately On This Occasssion You Have Not Won A Prize.";
-	private static final String STARTS_B_6 = "You Have Won $50";
-	private static final String STARTS_C_6 = "You Have Won $100";
-	private static final String STARTS_B_8 = "You Have Won $500";
-	private static final String STARTS_C_8 = "You Have Won $750";
-	private static final String STARTS_B_10 = "You Have Won $5000";
-	private static final String STARTS_C_10 = "You Have Won $10000";
-	private static final String FOUND_ERROR = "There Was An Unexpected Error";
+	@Mock
+	Constants constants;
+
 	@Before
 	public void setup() {
 		controller = new AccountPrizeController();
+		constants = new Constants();
 	}
 	
 	@Test
 	public void testDrawPrizeWithA() {
-		assertEquals(STARTS_A,controller.drawPrize("a085218071"));
+		assertEquals(constants.getSTARTS_A(),controller.drawPrize("a085218071"));
 	}
 
 	@Test
 	public void testDrawPrizeWithB6() {
-		assertEquals(STARTS_B_6,controller.drawPrize("b12212"));
+		assertEquals(constants.getSTARTS_B_6(),controller.drawPrize("b12212"));
 	}
 	@Test
 	public void testDrawPrizeWithB8() {
-		assertEquals(STARTS_B_8,controller.drawPrize("b3258449"));
+		assertEquals(constants.getSTARTS_B_8(),controller.drawPrize("b3258449"));
 	}
 	@Test
 	public void testDrawPrizeWithB10() {
-		assertEquals(STARTS_B_10,controller.drawPrize("b523287366"));
+		assertEquals(constants.getSTARTS_B_10(),controller.drawPrize("b523287366"));
 	}
 	@Test
 	public void testDrawPrizeWithC6() {
-		assertEquals(STARTS_C_6,controller.drawPrize("c96201"));
+		assertEquals(constants.getSTARTS_C_6(),controller.drawPrize("c96201"));
 	}
 	@Test
 	public void testDrawPrizeWithC8() {
-		assertEquals(STARTS_C_8,controller.drawPrize("c7915088"));
+		assertEquals(constants.getSTARTS_C_8(),controller.drawPrize("c7915088"));
 	}
 	@Test
 	public void testDrawPrizeWithC10() {
-		assertEquals(STARTS_C_10,controller.drawPrize("c352873583"));
+		assertEquals(constants.getSTARTS_C_10(),controller.drawPrize("c352873583"));
 	}
 	@Test
 	public void testDrawPrizeError() {
-		assertEquals(FOUND_ERROR,controller.drawPrize("d4660608"));
+		assertEquals(constants.getFOUND_ERROR(),controller.drawPrize("d4660608"));
 	}
 }
