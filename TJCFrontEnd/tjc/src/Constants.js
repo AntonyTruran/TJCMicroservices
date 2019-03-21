1041fca08b62e2aed6a76f96109bb32524a3f49d
smurfjames101 <smurfjames@gmail.com>
2019-03-21 09:59:14 +0000

    Created Misc Files
    Constants for better variable management
    Removed Logger from Application Properties

---
+++ TJCFrontEnd/tjc/src/Constants.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.FIRST_NAME = "http://localhost:8080/getLastAccountNames";
+exports.ACCOUNT_NUMBER = "http://localhost:8080/getLastAccountNumber";
+exports.CREATE_ACCOUNT = "";
+//# sourceMappingURL=Constants.js.map
---
+++ TJCFrontEnd/tjc/src/Constants.ts
@@ -0,0 +1,3 @@
+export const FIRST_NAME = "http://localhost:8080/getLastAccountNames";
+export const ACCOUNT_NUMBER = "http://localhost:8080/getLastAccountNumber";
+export const CREATE_ACCOUNT = "";
