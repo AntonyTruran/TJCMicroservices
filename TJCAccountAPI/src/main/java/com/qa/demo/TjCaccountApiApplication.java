a25169ab2eb3ca8427790aea4cd817c10a69aa31
BetaDraconis <lukeajones1996@gmail.com>
2019-03-22 11:07:50 +0000

    Fixed the things that the merge broke

--- TJCAccountAPI/src/main/java/com/qa/demo/TjCaccountApiApplication.java
+++ TJCAccountAPI/src/main/java/com/qa/demo/TjCaccountApiApplication.java
@@ -15,7 +15,7 @@ import com.qa.demo.entities.Account;
 @EnableJms
 public class TjCaccountApiApplication {
 
-	public static void main(String[] args) {
+	public static String main(String[] args) {
 		ApplicationContext ac = SpringApplication.run(TjCaccountApiApplication.class, args);
 		JmsTemplate jmsT = ac.getBean(JmsTemplate.class);
 		jmsT.convertAndSend("Jimmy is a weapon", new Account("Jimmy", "Weapon"));
